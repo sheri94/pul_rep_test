5561032b8ee3008e49b7b18f60616b99cf464038
sheri94 <eksinon@mail.ru>
2022-12-15 01:53:02 +0500

    3

---
+++ pulkovo/src/main/java/com/pulkovo/pulkovo/controller/DivisionController.java
@@ -0,0 +1,11 @@
+package com.pulkovo.pulkovo.controller;
+/**
+ \* Created with IntelliJ IDEA.
+ \* User: ProjectWin
+ \* Date: Andrey Volsky
+ \* Time: 1:15
+ \* To change this template use File | Settings | File Templates.
+ \* Description: 
+ \*/
+public class DivisionController {
+}
